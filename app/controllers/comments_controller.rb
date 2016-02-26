b23a00ad8bf2dc84384ef1467ae87734dbc2b619
Wepoo <yurkovetc@gmail.com>
2016-02-26 14:40:40 +0300

    Add nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,32 @@
 class CommentsController < ApplicationController
-  before_action :set_article, only: :create
+  # before_action :set_article, only: :create
+  after_action :set_parent, only: :create
+
+  def new
+    @reply_comment = Comment.find(params[:comment_id])
+    @comment = Comment.build_from( @reply_comment, current_user.id, '' )
+  end
 
   def create
     @comment = Comment.create( comment_params )
 
     respond_to do |format|
       format.js
-      if @comment.save
-        format.html { redirect_to @article, notice: 'Comment was successfully created.' }
-      else
-        format.html { redirect_to current_user }
-        #format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+      format.html
+  
     end
   end
 
   def comment_params
-  	params.require(:comment).permit(:commentable_id, :commentable_type, :user_id, :body, :parent_id)
+  	params.require(:comment).permit(:commentable_id, :commentable_type, :user_id, :parent_id, :body)
   end
 
   def set_article
   	@article = Article.find(params[:comment][:commentable_id])
   end
 
+  def set_parent
+    c = Comment.last
+    c.update parent_id: :commentable_id
+  end
 end
